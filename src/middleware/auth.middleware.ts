e4e2073a263f35af7f5933fdd8f01494752092cb
dire_wolf <aman.singhal@coditas.com>
2024-08-05 00:08:12 +0530

    implementing middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,9 +1,18 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { RequestService } from 'src/request.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly requestService: RequestService) {}
+
+  private readonly logger = new Logger(AuthMiddleware.name);
+
   use(req: Request, res: Response, next: NextFunction) {
+    this.logger.log(AuthMiddleware.name);
     const userId = '123';
+    this.requestService.setUserId(userId);
+
+    next();
   }
 }
