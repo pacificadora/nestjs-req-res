639a7bd86d117385c9246ea70f928eed0dd00c21
dire_wolf <aman.singhal@coditas.com>
2024-08-05 00:22:02 +0530

    implementing middleware

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RequestService } from './request.service';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+  constructor(private readonly requestService: RequestService) {}
   getHello(): string {
+    this.logger.log('get hello user id', this.requestService.getUserId());
     return 'Hello World!';
   }
 }
